7bcdb279db99ead1ec43ee23cb36a47bacec2c3e
cloudZQY <498219667@qq.com>
2017-12-21 18:17:32 +0800

    fea: add demo_01 demo_02

--- index.web.js
+++ index.web.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import React from 'react';
 import App from './src/App';
-console.log(App)
+
 AppRegistry.registerComponent('rnweb', () => App);
 AppRegistry.runApplication('rnweb', {
   initialProps: {
